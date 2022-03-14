d9d228bac3c41d5166eadb5e6a48219dad269926
sathvik848 <99522543+sathvik848@users.noreply.github.com>
2022-03-14 11:30:13 +0530

    Add files via upload
    Assignment 4

---
+++ JUnitAssignment4/src/com/cg/model/Calculator.java
@@ -0,0 +1,16 @@
+package com.cg.model;
+
+public class Calculator {
+
+	public static Integer multiply(int i, int j) {
+		// TODO Auto-generated method stub
+		return i*j;
+	}
+
+	public static Integer add(int i, int j) {
+		// TODO Auto-generated method stub
+		return i+j;
+	}
+	
+
+}
